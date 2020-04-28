bce613cbb44eb788941e8b7592199d734f022e73
Maciej Krzywda <maciej.krzywda@pl.abb.com>
2020-04-28 15:53:29 +0200

    Fix BB8

--- Pit_Escape.py
+++ Pit_Escape.py
@@ -7,7 +7,7 @@ robot = Robot()
 timestep = int(robot.getBasicTimeStep())
 
 # Max possible speed for the motor of the robot.
-maxSpeed = 20
+maxSpeed = 8.72
 
 gyro = robot.getGyro("body gyro")
 gyro.enable(timestep)
@@ -20,7 +20,7 @@ pitchMotor.setVelocity(1.85)
 # This is the time interval between direction switches.
 # The robot will start by going forward and will go backward after
 # this time interval, and so on.
-timeInterval = 3
+timeInterval = 3 # wieksze/mniejsze powoduje ze BB8 bedzie krecil sie na dnie i nigdy nie wyjdzie.
 
 # At first we go forward.
 forward = True
@@ -30,10 +30,12 @@ while robot.step(timestep) != 1:
     now = robot.getTime()
     # We check if enough time has elapsed.
     if now - lastTime > timeInterval :
-        # If yes, then we switch directions
+        # U mnie usunieta zmiana kierunku, atakujemy od razu w tym samym kierunku z ta sama predkoscia. 
         if forward:
             pitchMotor.setVelocity(maxSpeed)
+            print(gyro.getValues())
         else:
-            pitchMotor.setVelocity(maxSpeed)
+            pitchMotor.setVelocity(maxSpeed) 
+            print(gyro.getValues())
         forward = not forward
         lastTime = now
